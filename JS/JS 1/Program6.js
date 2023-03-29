86b064e17f687311d4bb53d41a4957d05839b0fb
AJ <joysondsouzaaj@gmail.com>
2023-03-29 11:31:03 +0530

    Operator

---
+++ JS/JS 1/Program6.js
@@ -0,0 +1,54 @@
+console.log("THE OPERATORS IN JAVA-SCRIPT");
+let a = 10
+let b = 3
+
+console.log("a + b  \t", a + b);
+console.log("a - b  \t", a - b);
+console.log("a * b  \t", a * b);
+console.log("a / b  \t", a / b);
+console.log("a % b  \t", a % b);
+console.log("a ** b  \t", a ** b);
+console.log("a++    \t", a++);
+console.log("++a  \t", ++a);
+console.log("a--  \t", a--);
+console.log("--a  \t", --a);
+
+
+console.log("\n\n\n___________________________\n\n\n");
+let assignmentOpr = 20
+console.log(assignmentOpr);
+
+assignmentOpr +=20
+console.log("assignment and increment",assignmentOpr);
+
+assignmentOpr *=2
+console.log("assignment and multiply",assignmentOpr);
+
+assignmentOpr /=2
+console.log("assignment and divide",assignmentOpr);
+
+assignmentOpr %=6
+console.log("assignment and reminder",assignmentOpr);
+
+assignmentOpr **=2
+console.log("assignment and raise to the power",assignmentOpr);
+
+
+console.log("\n\n Comparison Operator \n\n");
+
+let opr1 = 10
+let opr2 = "10"
+
+console.log("opr1 == opr2",opr1 == opr2);
+console.log("opr1 === opr2",opr1 === opr2);
+console.log("opr1 != opr2",opr1 != opr2);
+console.log("opr1 !== opr2",opr1 !== opr2);
+console.log("opr1 > opr2",opr1 > opr2);
+console.log("opr1 < opr2",opr1 < opr2);
+console.log("opr1 >= opr2",opr1 >= opr2);
+console.log("opr1 <= opr2",opr1 <= opr2);
+
+console.log("\n\n Logical OPerator");
+console.log("AND Operator - \t",true && false);
+console.log("OR  Operator - \t",true || false);
+console.log("NOT Operator - \t",!false);
