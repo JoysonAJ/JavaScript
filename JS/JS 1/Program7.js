cf2b47ce8b60e2e61af32b90700a3a1f698fb660
AJ <joysondsouzaaj@gmail.com>
2023-03-29 16:32:57 +0530

    Conditional Operators

---
+++ JS/JS 1/Program7.js
@@ -0,0 +1,38 @@
+const age = "20";
+console.log(typeof age);
+
+const numberAge = parseInt(age);
+console.log(typeof numberAge);
+console.log(numberAge);
+
+console.log("\n\n\n");
+if (numberAge < 0) {
+    console.log("Enter valid age");
+} else {
+    console.log("Age is valid");
+}
+// Switch case
+const date = new Date()
+switch (date.getDay()) {
+    case 0:
+        console.log("Sunday");
+        break;
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    case 3:
+        console.log("Wednesday");
+        break;
+    case 4:
+        console.log("Thursday");
+        break;
+    case 5:
+        console.log("Friday");
+        break;
+    case 6:
+        console.log("Saturday");
+        break;
+}
