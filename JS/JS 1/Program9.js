dd02418151416d8748fbd104326d1d3b8d1ad4ae
AJ <joysondsouzaaj@gmail.com>
2023-03-29 17:08:17 +0530

    for Loop

---
+++ JS/JS 1/Program9.js
@@ -0,0 +1,16 @@
+for(let i=0;i<5;i++){
+    console.log(i);
+}
+
+console.log("\n\n");
+const obj = {
+    course: "MCA",
+    Batch : "B",
+    year : "2",
+    ClassNo : 104
+}
+
+for(let a in obj){
+    console.log(`the object key value ${a} and object value is ${obj[a]}`);
+}
+
