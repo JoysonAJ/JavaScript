883cda91b4b0c879573996226959591fb84bf2ce
AJ <joysondsouzaaj@gmail.com>
2023-03-30 15:31:10 +0530

    Function in JS

---
+++ JS/JS 2/Program11.js
@@ -0,0 +1,16 @@
+console.log("Function in JavaScript");
+
+function addTwoNumber(a,b){
+    console.log(`Addition of ${a} and ${b} is ` );
+    return a+b
+}
+
+const display = (a) =>{
+    return a
+}
+
+console.log(addTwoNumber(1,2))
+console.log(addTwoNumber(2,2))
+console.log(addTwoNumber(4,5))
+
+console.log("Entered number is",display(45));
