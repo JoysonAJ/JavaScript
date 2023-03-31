d2529e55f3441b223cf30c0bb7108991146e9693
AJ <joysondsouzaaj@gmail.com>
2023-03-31 08:26:24 +0530

    Strings

---
+++ JS/JS 2/Program13.js
@@ -0,0 +1,16 @@
+const string1 = "HELLO"
+console.log(string1);
+console.log(string1.length);
+
+
+const string2 = 'Good Morning'
+console.log(string2);
+console.log(string2.length);
+
+const combineString = `${string1} , ${string2}`
+console.log(combineString);
+
+// Escape sequence Character
+
+const esp = "Good \rAfternoon"
+console.log(`\n\n ${esp}`);
