db4b3eac25ecb54e936ec4e1d03e428c23451386
AJ <joysondsouzaaj@gmail.com>
2023-04-08 08:58:14 +0530

    callbacks

---
+++ JS/JS6/Program52.js
@@ -0,0 +1,16 @@
+// // Synchronous
+// const name = "AJ"
+// const age = 52
+// const color = "Red"
+
+// console.log(`The ${name} who is ${age} age old with ${color} flag`);
+
+// // Asynchronous
+// console.log("\n\n ---------------------- \n\n");
+// console.log("Start");
+// setTimeout(() => {
+//     console.log("Hello ,from settimeout \n\n");
+// }, 3000);
+// console.log("END");
+
+// // Call backs
